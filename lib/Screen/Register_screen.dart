50298054756da82f6759cbf84b8a4f1a42632498
santosh <santoshchapagain55@gmail.com>
2021-07-05 23:24:55 +0545

    first

---
+++ lib/Screen/Register_screen.dart
@@ -0,0 +1,146 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:login_app/Screen/Login_screen.dart';
+import 'package:login_app/component/button.dart';
+import '../constant.dart';
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final formkey = GlobalKey<FormState>();
+  final _auth = FirebaseAuth.instance;
+  String email = '';
+  String password = '';
+  bool isloading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isloading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Form(
+              key: formkey,
+              child: AnnotatedRegion<SystemUiOverlayStyle>(
+                value: SystemUiOverlayStyle.light,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Color(0XffC2D3E0),
+                              Color(0X8CC2D3E0),
+                              Color(0X59C2D3E0),
+                              Color(0X1AC2D3E0)
+                            ]),
+                      ),
+                      child: SingleChildScrollView(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 120),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Sign UP",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 30),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (value) {
+                                email = value;
+                              },
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return "Please enter Email";
+                                }
+                              },
+                              textAlign: TextAlign.center,
+                              decoration: kTextFieldDecoration.copyWith(
+                                hintText: 'Enter Your Email',
+                                prefixIcon: Icon(
+                                  Icons.email,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            TextFormField(
+                              obscureText: true,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return "Please enter Password";
+                                }
+                              },
+                              onChanged: (value) {
+                                password = value;
+                              },
+                              textAlign: TextAlign.center,
+                              decoration: kTextFieldDecoration.copyWith(
+                                  hintText: 'Choose a Password',
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: Colors.black,
+                                  )),
+                            ),
+                            SizedBox(height: 30),
+                            SizedBox(height: 40),
+                            LoginSignupButton(
+                              title: 'Register',
+                              ontapp: () async {
+                                if (formkey.currentState!.validate()) {
+                                  try {
+                                    final newUser = await _auth
+                                        .createUserWithEmailAndPassword(
+                                            email: email, password: password);
+                                    if (newUser != null) {
+                                      await Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) => LoginScreen(),
+                                        ),
+                                      );
+                                    }
+                                  } on FirebaseAuthException catch (e) {
+                                    showDialog(
+                                      context: context,
+                                      builder: (ctx) => AlertDialog(
+                                        title: Text(' Ops! Regitration Failed'),
+                                        content: Text('${e.message}'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(ctx).pop();
+                                            },
+                                            child: Text('Okay'),
+                                          )
+                                        ],
+                                      ),
+                                    );
+                                  }
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+}
